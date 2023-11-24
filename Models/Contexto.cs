7d049f00ff026f6ffea817b03708c1b016373b96
pietra <15pietrasantos@email.com>
2023-11-24 08:32:09 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaLaboratorio.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+            //public Dbset<Paciente> Paciente {get; set;}
+            //public Dbset<Profissional> Profissional {get; set;}
+            //public Dbset<Cargo> Cargo {get; set;}
+            //public Dbset<Estado> Estado {get; set;}
+            //public Dbset<Cidade> Cidade {get; set;}
+            //public Dbset<Resultado> Resultado {get; set;}
+            //public Dbset<Exame> Exame {get; set;}
+        }
+    }
+}
+
+   
